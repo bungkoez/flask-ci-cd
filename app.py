3dc3db38944b1b0ac5cc1b468cece08e42630e9f
Steephan Riscy <stphnriscyturnip12@gmail.com>
2025-07-29 02:32:41 +0700

    update blog

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -9,3 +10,7 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # WAJIB GUNAKAN PORT DARI ENV
+    app.run(host="0.0.0.0", port=port)
