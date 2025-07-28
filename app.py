70f41c1a2f54c0afb5ff3f39893a526667ef2dce
Steephan Riscy <stphnriscyturnip12@gmail.com>
2025-07-29 06:48:24 +0700

    Fix typo: render_template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, render_templates
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_templates("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     # Railway memberi variabel PORT, gunakan itu
