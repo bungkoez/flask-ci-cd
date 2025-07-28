486e97c59ca7d4aeb056cd63f94d588b8e5370e0
Steephan Riscy <stphnriscyturnip12@gmail.com>
2025-07-29 02:49:39 +0700

    Fix Flask port binding and structure

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Railway akan memberikan PORT lewat environment
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
