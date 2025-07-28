ec75077c9aeec6a9da7e821d5d1865bfac2d2908
Steephan Riscy <stphnriscyturnip12@gmail.com>
2025-07-29 06:22:38 +0700

    update index blog dan style

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    # Untuk deployment di Railway, harus pakai host 0.0.0.0 dan port 8000
+    app.run(host='0.0.0.0', port=8000)
