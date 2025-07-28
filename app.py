797f1008792b8afa5a93e5cf3e6741da2b7f4d63
Steephan Riscy <stphnriscyturnip12@gmail.com>
2025-07-29 02:13:13 +0700

    ubah jadi blog

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    posts = [
-        {"title": "Selamat Datang", "content": "Ini adalah blog milik Steephan Riscy Pardingotan Turnip."},
-        {"title": "Postingan Pertama", "content": "Halo! Ini adalah postingan pertama saya di blog ini."},
-        {"title": "Apa itu Flask?", "content": "Flask adalah framework web Python yang ringan dan mudah digunakan."}
-    ]
-    return render_template("index.html", posts=posts)
+def index():
+    return render_template("index.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
