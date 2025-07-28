6a0120d0fb58fe00a5c616fb0ee9cd90554bab29
Steephan Riscy <stphnriscyturnip12@gmail.com>
2025-07-29 02:43:01 +0700

    update index blog dan style

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template("style.css")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # WAJIB GUNAKAN PORT DARI ENV
-    app.run(host="0.0.0.0", port=port)
-from flask import Flask, render_template
-import os
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")  # ini perlu file templates/index.html
-
-@app.route("/about")
-def about():
-    return render_template("about.html")  # ini juga
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
